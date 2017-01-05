f9f99a0222e5d26591b893789293483fcf97104b
kiark <fabio.boccacini@chiarcosso.com>
2017-01-05 02:09:18 -0800

    production migration

--- db/migrate/20161129170419_add_manufacturer_to_articles.rb
+++ db/migrate/20161129170419_add_manufacturer_to_articles.rb
@@ -1,6 +1,6 @@
 class AddManufacturerToArticles < ActiveRecord::Migration[5.0]
   def change
-    remove_column(:articles, :manufacturer_id)
+    # remove_column(:articles, :manufacturer_id)
     add_reference(:articles, :manufacturer, foreign_key: {to_table: :companies})
   end
 end
