5aef7fc365c8f3a429f1597ce84b0b2813de5ef8
kiark <fabio.boccacini@chiarcosso.com>
2017-01-05 02:11:45 -0800

    production migration2

--- db/migrate/20161130123027_correct_article_position_code.rb
+++ db/migrate/20161130123027_correct_article_position_code.rb
@@ -1,5 +1,5 @@
 class CorrectArticlePositionCode < ActiveRecord::Migration[5.0]
   def change
-    rename_column :articles, :postionCode, :position_code
+    # rename_column :articles, :postionCode, :position_code
   end
 end
