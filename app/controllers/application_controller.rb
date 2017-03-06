a075d394cb13f9e5d7d716456ad017b08708753e
kiark <fabio.boccacini@chiarcosso.com>
2017-03-06 02:30:13 -0800

    fixing devise problem - user person

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    byebug
-    devise_parameter_sanitizer.for(:account_update) << :person
+    # byebug
+    # devise_parameter_sanitizer.for(:account_update) << :person
   end
 
 end
