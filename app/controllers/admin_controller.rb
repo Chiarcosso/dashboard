8799305d9338dd03483e1ae05215ba623c73bcc1
kiark <fabio.boccacini@chiarcosso.com>
2017-04-07 08:10:09 -0700

    wip

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_admin
-  before_action :query_params, :only [:send_query]
+  before_action :query_params, only: [:send_query]
 
   def queries
     @list = Query.where(:model_class => 'Vehicle').first.nil?? '' : Query.where(:model_class => 'Vehicle').first.query
@@ -16,7 +16,7 @@ class AdminController < ApplicationController
 
   def query_params
     case params.require(:model_class)
-    when 'Vehicle':
+    when 'Vehicle'
       @query = %{SELECT Targa AS plate, f.RagioneSoc AS property, anno AS registrationDate,
               Marca AS manufacturer, Modello AS model, Telaio AS chassis, Km AS mileage,
               FROM Veicoli v
--- gitpull.sh
+++ gitpull.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 sudo service iptables stop
+sudo service nginx stop
 git pull
 sudo service iptables start
 RAILS_ENV=production bundle exec rake tmp:cache:clear
 RAILS_ENV=production bundle exec rake db:migrate
 bundle exec rvmsudo rake assets:precompile RAILS_ENV=production
-sudo service nginx restart
+sudo service nginx start
