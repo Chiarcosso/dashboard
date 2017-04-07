de69d95da1302eade0546f3fd438592542f16490
kiark <fabio.boccacini@chiarcosso.com>
2017-04-07 07:50:47 -0700

    pull script fixed

--- gitpull.sh
+++ gitpull.sh
@@ -3,7 +3,7 @@
 sudo service iptables stop
 git pull
 sudo service iptables start
-RAILS_ENV=production bundle-exec rake tmp:cache:clear
-RAILS_ENV=production bundle-exec rake db:migrate
-bundle-exec rvmsudo rake assets:precompile RAILS_ENV=production
+RAILS_ENV=production bundle exec rake tmp:cache:clear
+RAILS_ENV=production bundle exec rake db:migrate
+bundle exec rvmsudo rake assets:precompile RAILS_ENV=production
 sudo service nginx restart
