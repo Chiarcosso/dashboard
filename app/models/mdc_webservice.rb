c2e776b204d2670da6badacbf154da5efdcb79e2
kiark <fabio.boccacini@chiarcosso.com>
2017-05-30 06:50:59 -0700

    minor bugfix

--- app/models/mdc_webservice.rb
+++ app/models/mdc_webservice.rb
@@ -9,7 +9,7 @@ class MdcWebservice
     request.body = "<?xml version='1.0' encoding='UTF-8'?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Body><ns3:openSession xmlns:ns3=\"http://ws.dataexchange.mdc.gullivernet.com\"><ns3:useSharedDatabaseConnection>#{useSharedDatabaseConnection}</ns3:useSharedDatabaseConnection><ns3:username>#{username}</ns3:username><ns3:password>#{password}</ns3:password></ns3:openSession></soapenv:Body></soapenv:Envelope>"
     request.headers = {'Content-type': 'application/xop+xml; charset=UTF-8; type=text/xml', 'Content-Transfer-encoding': 'binary', 'Content-ID': '<0.155339ee45be667b7fb6bd4a93dfbdb675d93cb4dc97da9b@apache.org>'}
 
-    @sessionID = ::SessionID.new(HTTPI.postrequest).body.match(/<ax21:sessionID>(.*?)<\/ax21:sessionID>/)[1].to_s)
+    @sessionID = ::SessionID.new(HTTPI.postrequest).body.match(/<ax21:sessionID>(.*?)<\/ax21:sessionID>/)[1].to_s
 
   end
 
