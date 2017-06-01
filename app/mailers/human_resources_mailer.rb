e474e505eabfdba0fad5064c1c0371e7b378cb53
kiark <fabio.boccacini@chiarcosso.com>
2017-06-01 10:49:00 -0700

    byebug removed

--- app/mailers/human_resources_mailer.rb
+++ app/mailers/human_resources_mailer.rb
@@ -2,7 +2,7 @@ class HumanResourcesMailer < ApplicationMailer
 
   def vacation_request(application)
     @application = application
-    byebug
+    # byebug
     mail(to: 'fabio.boccacini@chiarcosso.com', subject: 'Richiesta '+application.type, attachments: [application.filename => application.form])
   end
 
