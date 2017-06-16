9a10ecf4822e660fc3c7da5388ae04f236fb215e
kiark <fabio.boccacini@chiarcosso.com>
2017-06-16 06:39:55 -0700

    bugfix

--- app/models/role.rb
+++ app/models/role.rb
@@ -10,5 +10,5 @@ class Role < ApplicationRecord
             :allow_nil => true
 
   scopify
-  scope :admin, ->(admin) { !admin ? where("name != 'admin'") }
+  scope :admin, ->(admin) { !admin ? where("name != 'admin'") : where('true')}
 end
