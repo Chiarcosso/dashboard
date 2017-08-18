5f7996be33309d203e6a0b5b7cfbb287ac407be3
kiark <fabio.boccacini@chiarcosso.com>
2017-08-18 06:30:19 -0700

    users index scrollable

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,7 +6,7 @@ class Person < ApplicationRecord
   has_many :company_relations, through: :relations
 
   scope :order_alpha, -> { order(:surname) }
-  scope :find_by_complete_name,->(name) { where("concat_ws(' ',surname,name) = ?", name) }
+  scope :find_by_complete_name,->(name) { where("concat_ws(' ',surname,name) LIKE ?", name) }
   scope :filter, ->(name) { where("name like ? or surname like ? or mdc_user like ? or ('mdc' like ? and mdc_user is not null and mdc_user != '')", "%#{name}%", "%#{name}%", "%#{name}%", "%#{name}%").order(:surname) }
   scope :mdc, -> { where("mdc_user is not null and mdc_user != ''") }
   scope :order_mdc_user, -> { order(:mdc_user)}
