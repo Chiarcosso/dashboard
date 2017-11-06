6519d98b97ff5976d093480dfa458e99e982565c
kiark <fabio.boccacini@chiarcosso.com>
2017-11-06 07:04:58 -0800

    bugfix

--- app/models/office.rb
+++ app/models/office.rb
@@ -18,7 +18,7 @@ class Office < ApplicationRecord
     unless article[0].nil?
       art_check = "and items.article_id = #{article[0].id}"
     end
-    if article[2]
+    # if article[2]
     list = Item.find_by_sql("select * from items inner join output_order_items on items.id = output_order_items.item_id inner join output_orders on output_orders.id = output_order_items.output_order_id where output_orders.id in (select id from output_orders where destination_type = 'Office' and destination_id = #{self.id}) #{art_check.to_s}")
     unless article[1].nil?
       res = list - article[1]
