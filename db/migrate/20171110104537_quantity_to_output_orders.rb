032e23bc94e2ec039c0ca1be0a6c7d1d16cc367a
kiark <fabio.boccacini@chiarcosso.com>
2017-11-16 10:33:38 -0800

    migration bugfix

--- db/migrate/20171110104537_quantity_to_output_orders.rb
+++ db/migrate/20171110104537_quantity_to_output_orders.rb
@@ -1,8 +1,8 @@
 class QuantityToOutputOrders < ActiveRecord::Migration[5.0]
   def change
-    change_column(:articles,:containedAmount,:decimal,precision:7,scale:2,null: false, default: 1)
-    add_column(:output_order_items,:quantity,:decimal,precision:7,scale:2,null: false, default: 1)
-    add_column(:items,:remaining_quantity,:decimal,precision:7,scale:2,null: false, default: 0)
+    change_column(:articles,:containedAmount,:decimal,precision:12,scale:3,null: false, default: 1)
+    add_column(:output_order_items,:quantity,:decimal,precision:12,scale:3,null: false, default: 1)
+    add_column(:items,:remaining_quantity,:decimal,precision:12,scale:3,null: false, default: 0)
 
     Item.unassigned.each do |i|
       i.update(remaining_quantity: i.article.containedAmount)
