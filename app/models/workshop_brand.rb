26bbfad45142b94fd8874072083d60890025807b
kiark <fabio.boccacini@chiarcosso.com>
2017-12-07 04:36:45 -0800

    minor bugfix

--- app/models/workshop_brand.rb
+++ app/models/workshop_brand.rb
@@ -1,6 +1,6 @@
 class WorkshopBrand < ApplicationRecord
-  resoucify
-  
+  resourcify
+
   belongs_to :workshop, foreign_key: :workshop_id, class_name: :company_address
   belongs_to :brand,foreign_key: :brand_id, class_name: :company
 end
