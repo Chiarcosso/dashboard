01b65a27407bb8b1e614c324fa82f3cde765beb4
kiark <fabio.boccacini@chiarcosso.com>
2017-12-07 03:30:29 -0800

    migration fix

--- db/migrate/20171123121416_create_geo_states.rb
+++ db/migrate/20171123121416_create_geo_states.rb
@@ -1,6 +1,10 @@
 class CreateGeoStates < ActiveRecord::Migration[5.0]
   def change
+    create_table :languages do |t|
+      t.string :name
 
+      t.timestamps
+    end
     create_table :geo_states do |t|
       t.string :name, null: false, index: true
       t.string :code, null: false, index: true
