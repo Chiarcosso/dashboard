748ec2d550e9bbbf4abf02e1afa8d9c9872cd632
kiark <fabio.boccacini@chiarcosso.com>
2018-03-06 16:23:31 +0100

    mdc bugfix

--- app/models/mdc_user.rb
+++ app/models/mdc_user.rb
@@ -5,9 +5,19 @@ class MdcUser < ApplicationRecord
   belongs_to :assigned_to_company, class_name: 'Company'
 
   scope :assigned, -> { where("assigned_to_person_id is not null or assigned_to_company_id is not null") }
-  
+
   def holder
     self.assigned_to_person.nil?? self.assigned_to_company : self.assigned_to_person
+  end
+
+  def complete_name
+    if self.assigned_to_person.nil?
+      self.assigned_to_company.complete_name
+    elsif self.assigned_to_company.nil?
+      self.assigned_to_person.complete_name
+    else
+      "#{self.assigned_to_person.complete_name} (#{self.assigned_to_company.complete_name})"
+    end
   end
 
   def self.find_by_holder string
