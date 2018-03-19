e84115b28ed84de6c642f9ebed0ab126d1eef0a8
kiark <fabio.boccacini@chiarcosso.com>
2018-03-19 15:20:39 +0100

    workshop bugfix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
 
   def dashboard
     if (current_user.has_role? 'officina') || (current_user.has_role? 'amministratore officina')
-      @search = {:opened => true, :closed => false, :plate => nil, :number => nil, :date_since => nil, :date_to => nil, :mechanic => nil}
-      view =  "workshop/index"
+      # @search = {:opened => true, :closed => false, :plate => nil, :number => nil, :date_since => nil, :date_to => nil, :mechanic => nil}
+      # view =  "workshop/index"
+      redirect_to '/worksheets/'
+      return 0
     else
       view = "_agenda"
     end
