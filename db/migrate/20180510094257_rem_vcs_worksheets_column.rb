70c70e9b0267385c07ad80b469643d08179343ff
kiark <fabio.boccacini@chiarcosso.com>
2018-05-15 18:03:36 +0200

    bugfix

--- db/migrate/20180510094257_rem_vcs_worksheets_column.rb
+++ db/migrate/20180510094257_rem_vcs_worksheets_column.rb
@@ -1,5 +1,5 @@
 class RemVcsWorksheetsColumn < ActiveRecord::Migration[5.0]
   def change
-    remove_reference :vehicle_check_sessions, :worksheets
+    remove_reference :vehicle_check_sessions, :worksheets if column_exists? :vehicle_check_sessions, :worksheets_id
   end
 end
