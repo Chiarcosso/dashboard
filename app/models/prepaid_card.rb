a7941236bcfb9186a5f4ac8b7c0afef93015dc1a
kiark <fabio.boccacini@chiarcosso.com>
2018-05-29 14:19:06 +0200

    prepaid cards colored expiration

--- app/models/prepaid_card.rb
+++ app/models/prepaid_card.rb
@@ -5,7 +5,16 @@ class PrepaidCard < ApplicationRecord
   scope :filter, ->(search) { joins('left join people on people.id = prepaid_cards.person_id').where("serial like ? or concat(people.name,' ',people.surname) like ? or concat(people.surname,' ',people.name) like ?","%#{search.to_s.tr('*','%')}%","%#{search.to_s.tr('*','%')}%","%#{search.to_s.tr('*','%')}%") }
   scope :person_alpha_order, -> { joins('left join people on people.id = prepaid_cards.person_id').order('people.surname asc, people.name asc') }
   scope :active, ->(active) { active.nil?? where('1') : where(:dismissed => !active) }
+
   def is_valid?
     !self.dismissed && self.expiring_date >= Date.today
+  end
+
+  def expiration_style
+    if self.expiring_date == Date.today
+      'background-color: #ff9;'
+    elsif self.expiring_date < Date.today
+      'background-color: #f99;'
+    end
   end
 end
