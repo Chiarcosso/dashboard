727e25bb634beda1bee2b05d67c8c10d2871465f
kiark <fabio.boccacini@chiarcosso.com>
2018-07-20 13:29:47 +0200

    edits

--- timestamp_script.sh
+++ timestamp_script.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 source /etc/profile
+
+#temporary disable firewall
+service iptables stop
+
 #mount shared folder
 mount.cifs $RAILS_CAME_HOST $RAILS_CAME_LOCAL -o user=$RAILS_CAME_USER,pass=$RAILS_CAME_PASS,ro,sec=ntlm,iocharset=utf8,uid=dashboard
 
@@ -12,3 +16,6 @@ fi
 
 #unmount the folder
 umount $RAILS_CAME_LOCAL
+
+#reenable firewall
+service iptables start
