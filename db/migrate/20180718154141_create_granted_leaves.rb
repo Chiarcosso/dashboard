6ed44f07fe9f015fdffc04990050a616fa943a43
kiark <fabio.boccacini@chiarcosso.com>
2018-07-20 13:10:53 +0200

    debug

--- db/migrate/20180718154141_create_granted_leaves.rb
+++ db/migrate/20180718154141_create_granted_leaves.rb
@@ -1,14 +1,16 @@
 class CreateGrantedLeaves < ActiveRecord::Migration[5.0]
   def change
     drop_table :granted_leafes if table_exists? :granted_leafes
-    create_table :granted_leaves do |t|
-      t.references :leave_code, foreign_key: true, null: false, index: true
-      t.references :person, foreign_key: true, null: false, index: true
-      t.date :date, index: true
-      t.datetime :from, null: false
-      t.datetime :to, null: false
+    unless table_exists? :granted_leaves
+      create_table :granted_leaves do |t|
+        t.references :leave_code, foreign_key: true, null: false, index: true
+        t.references :person, foreign_key: true, null: false, index: true
+        t.date :date, index: true
+        t.datetime :from, null: false
+        t.datetime :to, null: false
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
