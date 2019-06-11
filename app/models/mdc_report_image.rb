451b6d4279a6619b3047a09967f362ce102cf355
kiark <fabio.boccacini@chiarcosso.com>
2019-06-11 10:15:34 +0200

    bug mdc report image url fixed

--- app/models/mdc_report_image.rb
+++ app/models/mdc_report_image.rb
@@ -2,6 +2,11 @@ class MdcReportImage < ApplicationRecord
   belongs_to :mdc_report
 
   def complete_url
-    "http://#{ENV['RAILS_IIS_URL']}#{self.url}"
+    url = self.url
+    if /https?:\/\/.*/ =~ url
+      return self.url
+    else
+      return "http://#{ENV['RAILS_IIS_URL']}#{self.url}"
+    end
   end
 end
