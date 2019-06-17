638571ca0b9831d07e42811fd59a4cfc04a27146
kiark <fabio.boccacini@chiarcosso.com>
2019-06-17 23:24:48 +0200

    home controller modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,20 @@
 class HomeController < ApplicationController
 
   def dashboard
-    if (current_user.has_role? 'officina') || (current_user.has_role? 'amministratore officina')
+    if (current_user.has_role? 'odl aperti') || (current_user.has_role? 'amministratore officina')
       # @search = {:opened => true, :closed => false, :plate => nil, :number => nil, :date_since => nil, :date_to => nil, :mechanic => nil}
       # view =  "workshop/index"
       redirect_to '/worksheets/'
       return 0
-    elsif (current_user.has_role? 'lavaggio')
+
+
+    elsif (current_user.has_role? 'lavaggio') || (current_user.has_role? 'checkup point')
       redirect_to '/carwash/checks/'
       return 0
-    elsif (current_user.has_role? 'personale')
+    elsif (current_user.has_role? 'magazzino')
+      redirect_to '/storage/'
+      return 0
+    elsif (current_user.has_role? 'presenze e orari')
       redirect_to '/presence/manage/'
       return 0
     else
